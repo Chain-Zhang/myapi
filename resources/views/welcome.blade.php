c85bc210a79958886554a75f08ad22a7c1d6afff
chain-zhang <zzceng@126.com>
2017-07-20 19:03:03 +0800

    update home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>API 集成平台</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,15 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Api Platform
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a title="测试 api" href="javascript: ;">Test Api</a>
+                    <a title="音乐 api" href="javascript: ;">Music Api</a>
+                    <a title="电影 api" href="javascript: ;">Movie Api</a>
+                    <a title="快递 api" href="javascript: ;">Delivery Api</a>
+                    <a title="天气 api" href="javascript: ;">Weather Api</a>
                 </div>
             </div>
         </div>
